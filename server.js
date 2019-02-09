b2ce40e06cb2cfb6ff8fae30f63e174f96653dec
Devin Udy <devin.udy@gmail.com>
2019-02-09 14:43:22 -0700

    Added express, http, and path packages. Created HTML for home and survey pages

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 var inquirer = require('inquirer');
 var fs = require('fs');
+var express = require('express');
+var path = require('path');
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("Server is listening on PORT: " + PORT);
+});
