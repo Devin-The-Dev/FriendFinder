cc49f373587770ab086f5c9225f40f6747c4afb9
Devin Udy <devin.udy@gmail.com>
2019-02-11 23:20:46 -0700

    Added body parser package. Fixed error in friends.js. Added questions in survey.html

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-var inquirer = require('inquirer');
-var fs = require('fs');
+//var inquirer = require('inquirer');
+//var fs = require('fs');
 var express = require('express');
 var path = require('path');
-var app = express();
+//var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
 
-require("./app/routing/htmlRoutes")(app);
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+//HTML Routes
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function () {
     console.log("Server is listening on PORT: " + PORT);
