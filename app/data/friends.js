716dfc7757806c9b0a7319ca0a052c2c9db224d3
Devin Udy <devin.udy@gmail.com>
2019-03-20 12:46:23 -0600

    Changed friend photos, changed questions, had friend appear after submiting results

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@
 var friendsArray = [
     {
         "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiZvIvzqpHhAhVK2IMKHVbnDesQjRx6BAgBEAU&url=https%3A%2F%2Fcatalystfdg.com%2Fabout-us%2Fmeet-the-team%2Fwade-headshot%2F&psig=AOvVaw2QWrjXYhREngvNcVsYGMPw&ust=1553192396827957",
         "scores": [
             5,
             1,
@@ -19,32 +19,32 @@ var friendsArray = [
     },
     {
         "name": "Lour Ritter",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://static1.squarespace.com/static/55d7222de4b07fe20b7babbf/593c217137c58172ed3b9e96/593c2a3bd2b857459a5217b8/1497115342841/Arlington-Corporate-Headshot.jpg?format=300w",
         "scores": [
-            4,
-            3,
-            4,
             1,
-            5,
             2,
+            3,
+            4,
             5,
             1,
+            2,
             3,
-            4
+            4,
+            5
         ]
     },
     {
         "name": "Jordan Biason",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://format-com-cld-res.cloudinary.com/image/private/s--8i-PDyff--/c_fill,g_rek_faces,h_300,w_300/a_auto,fl_keep_iptc.progressive,q_95/v1/e3504fc13ec36a8f20e7a2442212dfe2/026_HilmaCastillo_Headshot.jpg",
         "scores": [
-            4,
+            3,
+            5,
+            2,
+            1,
             4,
             2,
             3,
-            2,
-            2,
             3,
-            2,
             4,
             5
         ]
